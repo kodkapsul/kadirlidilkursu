c8c116ce36274b2bb4f71c2ff121a14a3f91e8a7
Semih CINKIR <csemih@gmail.com>
2025-06-25 17:01:41 +0300

    hakkında ve iletişim sayfası yapımına başlandı

--- src/app/(routes)/about/page.tsx
+++ src/app/(routes)/about/page.tsx
@@ -1,9 +1,56 @@
+
+import Image from "next/image";
 import React from "react";
 
 const aboutPage =() => {
     return ( 
-<div>Hakkımızda sayfası hazırlanıyor</div>
+    <>
+    <header className="section documentation">
+    <div className="container">
+    <div className="columns is-desktop is-vcentered">
+        <div className="column is-half">
+            <h1>Kendinizi Geliştirin.</h1>
+            <p className="body1">
+                    Kadirli Dil Kursu - Hakkımızda </p>
+        </div>
+        <div className="column is-half">
+            <Image src="/assets/images/header/header-about.png" alt="" width={1136} height={726}/>
+        </div>
+    </div>
+    </div>
+    </header>
 
+    <section className="section docs">
+	    <div className="container">
+		    <div className="columns">
+			    <div className="column is-6 has-text-right">
+				    <Image src="/assets/images/London-travel.png" alt="" width={443} height={687}/>
+			    </div>
+          <div className="column is-6">
+            <h3>Kadirli Dil Kursu</h3>
+            <p className="decription">
+            Kadirli Dil Kursu, asıl meslekleri öğretmenlik olan Türk ve Yabancı eğitmenlerden oluşan uzman eğitim ve 
+            yönetim kadrosu ile gelişen teknolojiyi takip ederek sektördeki saygın ve güvenilir konumunu her yıl güçlendirirken, 
+            bir yandan da profesyonel ve kurumsal yaklaşım ile kaliteli hizmet vermeyi amaç edinmiştir. <br /><br />
+            </p>
+            <p className="decription">
+            Kadirli Dil Kursu, öğrencilere evrensel bir dil olan İngilizce'yi etkili ve hızlı bir şekilde öğrenme fırsatı sunmaktadır. 
+            Sunduğumuz eğitim metotları, öğrencilere kalıcı ve sıkıcı olmayan bir öğrenme deneyimi yaşatır, 
+            bu da öğrendiklerinin uzun yıllar boyunca unutulmamasını sağlar. <br /><br />
+            </p>
+            <p className="decription">
+            Kadirli Dil Kursu, öğrencilerini sürekli olarak izlemekte ve dil becerilerinin ilerlemesini desteklemek amacıyla 
+            özel olarak hazırlanmış bir sistem uygulamaktadır. 
+            Bu sistem, her öğrencinin ihtiyaçlarına özgü çözüm önerileri sunarak bireysel başarıyı hedeflemektedir. 
+            Öğrencilerimize sunduğumuz bu kişiselleştirilmiş yaklaşım, dil öğrenme deneyimlerini daha etkili ve tatmin edici hale getirirken, 
+            onları geleceğe hazırlamak için önemli bir rol oynamaktadır.<br /><br />
+
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    </>
     )
 }
 
--- src/app/(routes)/contact/page.tsx
+++ src/app/(routes)/contact/page.tsx
@@ -1,10 +1,38 @@
+import Image from 'next/image'
 import React from 'react'
 
 const contactPage = () => {
   return (
-    <div>
-      İletişim sayfası hazırlanıyor
-    </div>
+    <>
+    <header className="section jobs-header">
+      
+        <div className="columns is-desktop is-vcentered">
+          <div className="column is-half">
+            <h1>İletişime Geçin</h1>
+            <p className="body1">
+              Kadirli Dil Kursu - İletişim
+            </p>
+          </div>
+          <div className="column is-half">
+          <Image src="/assets/images/header/header-contact.png" alt="" width={1158} height={748}/>
+          </div>
+        </div>
+	  </header>
+
+    <section className="section docs">
+	    <div className="container">
+		    <div className="columns">
+			    
+          <div className="column is-full">
+				    <h2>Hazırlanıyor</h2>
+				    <p className="body1">
+					     --
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+   </>
   )
 }
 
