798e7813c19c9ff3be89df305dc199e626e99c00
umaytek <umaytek@umaytek-Mac-mini.local>
2026-06-16 01:48:30 +0300

    🐛 Fix: Change allowFullScreen to boolean value

--- src/providers/map.tsx
+++ src/providers/map.tsx
@@ -14,7 +14,7 @@ export function Map() {
           width="100%"
           height="500" 
           style={{"border":0}} 
-          allowFullScreen=""
+          allowFullScreen={true}
           loading="lazy" 
           referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
